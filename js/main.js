8d46bf14ba8e0807a608b0faee5d00e709bf03ff
Konstantin <maschkovc@yandex.ru>
2023-04-22 17:48:23 +0300

    render

--- js/main.js
+++ js/main.js
@@ -155,13 +155,14 @@ const modaltrigger = document.querySelectorAll('[data-modal]'),
     // используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
             this.parentSelector = document.querySelector(parentSelector);
+            this.classes = classes;
             this.transfer = 27
             this.changToUAH();
         }
@@ -170,8 +171,15 @@ const modaltrigger = document.querySelectorAll('[data-modal]'),
         }
         render() {
             const element = document.createElement('div');
+            
+            if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-            <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -180,7 +188,6 @@ const modaltrigger = document.querySelectorAll('[data-modal]'),
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span>$/день</div>
                 </div>
-            </div>
             `;
             this.parentSelector.append(element);
         }
@@ -192,7 +199,9 @@ const modaltrigger = document.querySelectorAll('[data-modal]'),
         `Меню "Фитнес"`,
         `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
         9,
-        `.menu .container`
+        `.menu .container`,
+        `menu__item`,
+        `big`,
     ).render();
 
     new MenuCard(
@@ -201,7 +210,9 @@ const modaltrigger = document.querySelectorAll('[data-modal]'),
         `Меню “Премиум”`,
         `В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
         12,
-        `.menu .container`
+        `.menu .container`,
+        `menu__item`,
+        
     ).render();
 
     new MenuCard(
@@ -210,7 +221,8 @@ const modaltrigger = document.querySelectorAll('[data-modal]'),
         `Меню "Постное"`,
         `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.`,
         10,
-        `.menu .container`
+        `.menu .container`,
+        `menu__item`,
     ).render();
 
 });
